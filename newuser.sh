e8eaacea4feb02236d63996c4c3048bb0fb1893d
Michael Cox <michael@se-make.co.uk>
2015-05-26 22:55:10 +0100

    Update newuser.sh

--- newuser.sh
+++ newuser.sh
@@ -2,9 +2,19 @@
 
 username=$1
 domain=$2
+
+# setup new user with home dir
 useradd $username -s /bin/bash -d /var/www/$domain -m
+
+# in group www-data
 usermod -a -G www-data $username
-chown -R $username:www-data "/var/www/$domain"
-sudo chmod -R g+s "/var/www/$domain"
+
+# set password
 passwd $username
+
+# create virtual host
 virtualhost create $domain $domain/httpdocs
+
+# change permissions
+chown -R $username:www-data "/var/www/$domain"
+sudo chmod -R 755 "/var/www/$domain"
